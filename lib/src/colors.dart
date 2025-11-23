f5473bb7fd14318b42cff5e22c72481a00cad6e9
Ladino Anselmo <ladans.me@gmail.com>
2025-11-23 15:51:40 +0200

    ref: ui/ux

--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -7,4 +7,4 @@ const Color gray = Color(0xFF5E5E5E);
 const Color green = Color(0xFF53C667);
 final Color amber = Colors.amber;
 final Color red = Colors.red;
-final destructiveRed = Platform.isAndroid ? Colors.red.shade700 : CupertinoColors.destructiveRed;
+final destructiveRed = Platform.isAndroid ? Colors.red : CupertinoColors.destructiveRed;
