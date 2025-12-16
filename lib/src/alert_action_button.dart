265f3e801a111447968b9df6c4212f6b628d9040
Ladino Anselmo <ladans.me@gmail.com>
2025-12-16 17:57:23 +0200

    fix: positive color

--- lib/src/alert_action_button.dart
+++ lib/src/alert_action_button.dart
@@ -35,7 +35,7 @@ class AlertActionButton extends StatelessWidget {
   }) {
     final backgroungColor = isDestructive 
     ? destructiveRed 
-    : Theme.of(context).primaryColor.withValues(alpha: isPositive ? .4 : 1);
+    : Theme.of(context).primaryColor.withValues(alpha: isPositive ? .6 : 1);
 
     return PressEffect(
       onPressed: onPressed,
