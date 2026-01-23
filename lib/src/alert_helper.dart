d7cb228093b9342de23ec3c4b04c33b387abc6cd
Ladino Anselmo <ladans.me@gmail.com>
2026-01-23 20:07:44 +0200

    fix; alert scrollview

--- lib/src/alert_helper.dart
+++ lib/src/alert_helper.dart
@@ -217,14 +217,21 @@ Future<T?> _showDialog<T>(
     context: context ?? Flashly.context,
     barrierDismissible: false,
     barrierColor: Colors.black45,
-    builder: (context) => Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List<Widget>.generate(
-        1, (index) => _AlertContainer(
-          asLoader: asLoader,
-          radius: radius,
-          child: SingleChildScrollView(child: buildChild(context)),
+    builder: (context) => SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List<Widget>.generate(
+            1, (index) => _AlertContainer(
+              asLoader: asLoader, 
+              radius: radius,
+              child: buildChild(context), 
+            ),
+          ),
         ),
       ),
     ),
